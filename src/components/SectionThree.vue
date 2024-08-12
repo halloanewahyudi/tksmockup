1ba72b3b5fc5bcf88d651dec7c8d7e5afa7b65a6
halloanewahyudi <30697981+halloanewahyudi@users.noreply.github.com>
2024-08-12 17:37:37 +0700

    rev lagi

--- src/components/SectionThree.vue
+++ src/components/SectionThree.vue
@@ -4,8 +4,11 @@ import ArrowRight from "./icons/ArrowRight.vue";
 <template>
   <section class="secthree flex justify-end items-center min-h-screen pt-20 pb-40 relative">
     <div class="max-w-[720px] p-6 lg:p-12 flex flex-col gap-4">
-      <p class="text-primary-700 tracking-wide"> Our Pproducts</p>
-      <h4 class="text-primary-950 font-semibold">SWIT thermal Stratification</h4>
+      <div>
+        <p class="text-secondary-600 tracking-wide text-lg font-medium"> Our Products</p>
+        <h4 class="text-primary-950 font-semibold">SWIT thermal Stratification</h4>
+      </div>
+    
       <p class="text-xl ">
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
       </p>
@@ -23,7 +26,7 @@ import ArrowRight from "./icons/ArrowRight.vue";
 <style scoped>
  
 .secthree {
-  background: url('../assets/bg-sec3.png')no-repeat -280px center;
+  background:#f2f2f2 url('../assets/swit.jpg')no-repeat -280px center;
 
 }
 
